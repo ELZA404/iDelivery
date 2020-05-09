21c16a440c72a1751c3acafd7c2f9bbc12ef1bfa
DanyloAntonenko <danylo.antonenko@nure.ua>
2020-05-09 16:03:57 +0300

    Initial commit

---
+++ iDelivery/src/main/java/com/nure/antonenko/iDelivery/IDeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.nure.antonenko.iDelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IDeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IDeliveryApplication.class, args);
+	}
+
+}
